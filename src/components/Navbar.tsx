44d019d2f895db8f8107e1f46c57ad0b2e0dc4d7
Keiji Lohier <keiji.lohier@gmail.com>
2026-02-26 14:06:23 -0500

    clean up comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,16 @@
 import { Link } from "react-router-dom";
 
-
-
-
-export default function Navbar(){
-return(
+export default function Navbar() {
+  return (
     <nav>
-            <ul>
-                {/* <li className="fa-solid fa-dice-d20 fa-2x"></li> */}
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/chosen">Chosen</Link></li>
-                {/* <li><a href="favorite.html" className="desktop-nav fa-solid fa-star">Chosen</a></li> */}
-                {/* <li><a href="#" className="desktop-nav fa-solid fa-user">Log In</a></li>? */}
-            {/* <li id="icon" className="hamburger-menu fa-solid fa-bars fa-2x"></li> */}
-            </ul>
-            
-        </nav>
-)
-
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/chosen">Chosen</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
